423852991b2d6c152c5520d7c7ff035082d4b14d
carlosfdezb <carlosfdezb@gmail.com>
2026-05-21 22:50:02 +0000

    feat(ux): add scroll restoration between page navigations

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, ScrollRestoration } from 'react-router-dom';
 import App from './App';
 import { TVFocusProvider } from './context/TVFocusContext';
 import './styles/globals.css';
@@ -10,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <BrowserRouter>
+        <ScrollRestoration />
         <TVFocusProvider>
           <App />
         </TVFocusProvider>
