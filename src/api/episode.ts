0c9580fd576745a15d3434781c05ebd42945d74f
carlosfdezb <carlosfdezb@gmail.com>
2026-07-08 22:08:15 +0000

    test 2 lazygit

--- src/api/episode.ts
+++ src/api/episode.ts
@@ -1,6 +1,7 @@
-import { apiFetch } from './client';
-import type { EpisodeDetail } from '../types/api';
+import { apiFetch } from "./client";
+import type { EpisodeDetail } from "../types/api";
 
 export async function getEpisode(slug: string, number: number): Promise<EpisodeDetail> {
+  console.log("esto es un test");
   return apiFetch<EpisodeDetail>(`/episode/${slug}/${number}`);
 }
